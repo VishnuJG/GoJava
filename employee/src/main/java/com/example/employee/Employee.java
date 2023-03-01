f27818b168d647defdb58c154ede2453f9b467b2
VishnuJG <jgvishnu2001@gmail.com>
2023-03-01 11:11:34 +0530

    fix(javaserver): the package messup fixes
    Error arrising due to change in package name fixed, now works with just clone, no requirement for
    updating the pom file. There was a
    <start-class>com.example.employee.EmployeeApplication</start-class> required in the pom properties
    div. This was a fix for the no main class found in the following candidates error.

--- employee/src/main/java/com/example/employee/Employee.java
+++ employee/src/main/java/com/example/employee/Employee.java
@@ -1,4 +1,4 @@
-package Tests.employee;
+package com.example.employee;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.annotation.Id;
 import org.bson.types.ObjectId;
--- employee/src/main/java/com/example/employee/EmployeeRepository.java
+++ employee/src/main/java/com/example/employee/EmployeeRepository.java
@@ -1,9 +1,9 @@
-package Tests.employee;
-// import springboot.api.employee.*; 
+package com.example.employee;
+
 import org.bson.types.ObjectId;
-// import com.example
+
 import org.springframework.data.mongodb.repository.MongoRepository;
-import java.util.*;
+
 public interface EmployeeRepository extends MongoRepository<Employee2, ObjectId> {
     Employee findById(int id);
     void deleteById(int id);
